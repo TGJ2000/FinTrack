84e96023ed9b8d2de2dfbe666e32e135665c8c84
TGJ2000 <joelwhite83@gmail.com>
2026-03-16 23:07:46 -0500

    Feature - Added update and delete endpoints for transactions. Feature - Created DTOs for CreateTransaction and UpdateTransaction. Refactor - Changed TransactionRepository to use a primary constructor.

--- FinTrack/Controllers/TransactionController.cs
+++ FinTrack/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
-﻿using FinTrack.Models;
+﻿using FinTrack.DTOs;
+using FinTrack.Models;
 using FinTrack.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,36 +7,48 @@ namespace FinTrack.Controllers
 {
     [ApiController]
     [Route(template: "api/transactions")]
-    public class TransactionController : ControllerBase
+    public class TransactionController(TransactionRepository transactionRepository) : ControllerBase
     {
-        private TransactionRepository _repo;
-
-        public TransactionController(TransactionRepository transactionRepository)
-        {
-            _repo = transactionRepository;
-        }
-
         [HttpGet]
         public async Task<IActionResult> GetUserTransactions(DateOnly? startDate, DateOnly? endDate, string? type, int? page, int? pageSize)
         {
             int user = 1;
-            List<Transaction> userTransactions = await _repo.GetTransactions(user, startDate, endDate, type, page, pageSize);
+            List<Transaction> userTransactions = await transactionRepository.GetTransactions(user, startDate, endDate, type, page, pageSize);
             return Ok(userTransactions);
         }
-        [Route("api/summary")]
+        [Route("summary")]
         [HttpGet]
-        public async Task<IActionResult> GetTransactionSummary(DateOnly ?startDate, DateOnly? endDate)
+        public async Task<IActionResult> GetTransactionSummary(DateOnly? startDate, DateOnly? endDate)
         {
             int user = 1;
-            List<TransactionSummary> transactionSummary = await _repo.GetTransactionSummary(user, startDate, endDate);
+            List<TransactionSummary> transactionSummary = await transactionRepository.GetTransactionSummary(user, startDate, endDate);
             return Ok(transactionSummary);
         }
-        [Route("api/transactions/create")]
+        [Route("create")]
         [HttpPost]
-        public async Task<IActionResult> CreateTransaction(int categoryId, decimal amount, string type, string description, DateOnly? transactionDate)
+        public async Task<IActionResult> CreateTransaction([FromBody] CreateTransactionDto transaction)
         {
             int user = 1;
-            await _repo.CreateTransaction(user, categoryId, amount, type, description, transactionDate);
+            await transactionRepository.CreateTransaction(user, transaction);
+            return StatusCode(201);
+        }
+
+        [Route("{transactionId}")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateTransaction(int transactionId, [FromBody] UpdateTransactionDto transaction)
+        {
+            int user = 1;
+            await transactionRepository.UpdateTransaction(userId: user, transactionId, transaction);
+            return Ok();
+        }
+
+
+        [Route("{transactionId}")]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteTransaction(int transactionId)
+        {
+            int user = 1;
+            await transactionRepository.DeleteTransaction(user, transactionId);
             return Ok();
         }
     }
